0e7de32881b508990b5b5de826540d9ad0611fe2
R5meason <melynda.eason@gmail.com>
2016-12-09 12:56:32 -0800

    updated contact method

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDkSPjgMHnma13HxpssmNrU4JyEB_652jc",
+    authDomain: "messagecenter-2ba1e.firebaseapp.com",
+    databaseURL: "https://messagecenter-2ba1e.firebaseio.com",
+    storageBucket: "messagecenter-2ba1e.appspot.com",
+    messagingSenderId: "107256469130"
+};
+
+firebase.initializeApp(config);
+var fbRef = firebase.database();
+
 $(window).load(function () {
     $('.displayInfo').slideDown("slow");
 });
@@ -27,9 +39,30 @@ $(document).ready(function () {
     });
 });
 
+var validEmail = false;
+
 var emailCheck = function () {
     if (!$('#emailAddy').val().includes("@") || !$('#emailAddy').val().includes(".com")) {
         console.log("no");
         document.getElementById("validEmail").innerHTML = "Please Enter a Valid Email Address";
+    }
+    else {
+        validEmail = true;
+        console.log("yes");
+    }
+};
+
+var sendMsg = function (em, msg) {
+    emailCheck();
+    if (validEmail) {
+        var data = {
+            em: em,
+            msg: msg
+        };
+        fbRef.ref('msg').push(data);
+        document.getElementById('emailAddy').value = "";
+        document.getElementById('msg').value = "";
+        document.getElementById("validEmail").innerText = "You Message Has Been Sent!";
+        validEmail = false;
     }
 };
