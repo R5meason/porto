7e3ca61a914b839cd71b704ec3e0ac4d876df8e3
R5meason <melynda.eason@gmail.com>
2016-12-11 19:48:12 -0800

    added char limit to msgs and modded script based on fb limit

--- script.js
+++ script.js
@@ -51,19 +51,109 @@ var emailCheck = function () {
     }
 };
 
+var proceed = function () {
+    document.getElementById('emailAddy').value = "";
+    document.getElementById('msg').value = "";
+    document.getElementById("validEmail").innerText = "Your Message Has Been Sent!";
+    validEmail = false;
+    validMsg = false;
+};
+
 var sendMsg = function (em, msg) {
     emailCheck();
-    if (validEmail && $('#msg').val().length > 0) {
+
+    if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 21) {
         var data = {
             em: em,
             msg: msg
         };
         fbRef.ref('msg').push(data);
-        document.getElementById('emailAddy').value = "";
-        document.getElementById('msg').value = "";
-        document.getElementById("validEmail").innerText = "Your Message Has Been Sent!";
-        validEmail = false;
-        validMsg = false;
+        proceed();
+    }
+    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 41) {
+        var data = {
+            em: em,
+            msg: msg.substring(0, 19),
+            msg2: msg.substring(19)
+        };
+        fbRef.ref('msg').push(data);
+        proceed();
+    }
+    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 61) {
+        var data = {
+            em: em,
+            msg: msg.substring(0, 19),
+            msg2: msg.substring(19, 39),
+            msg3: msg.substring(39)
+        };
+        fbRef.ref('msg').push(data);
+        proceed();
+    }
+
+    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 81) {
+        var data = {
+            em: em,
+            msg: msg.substring(0, 19),
+            msg2: msg.substring(19, 39),
+            msg3: msg.substring(39, 59),
+            msg4: msg.substring(59)
+        };
+        fbRef.ref('msg').push(data);
+        proceed();
+    }
+    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 101) {
+        var data = {
+            em: em,
+            msg: msg.substring(0, 19),
+            msg2: msg.substring(19, 39),
+            msg3: msg.substring(39, 59),
+            msg4: msg.substring(59, 79),
+            msg5: msg.substring(79)
+        };
+        fbRef.ref('msg').push(data);
+        proceed();
+    }
+    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 121) {
+        var data = {
+            em: em,
+            msg: msg.substring(0, 19),
+            msg2: msg.substring(19, 39),
+            msg3: msg.substring(39, 59),
+            msg4: msg.substring(59, 79),
+            msg5: msg.substring(79, 99),
+            msg6: msg.substring(99)
+        };
+        fbRef.ref('msg').push(data);
+        proceed();
+    }
+    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 141) {
+        var data = {
+            em: em,
+            msg: msg.substring(0, 19),
+            msg2: msg.substring(19, 39),
+            msg3: msg.substring(39, 59),
+            msg4: msg.substring(59, 79),
+            msg5: msg.substring(79, 99),
+            msg6: msg.substring(99, 119),
+            msg7: msg.substring(119)
+        };
+        fbRef.ref('msg').push(data);
+        proceed();
+    }
+    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 161) {
+        var data = {
+            em: em,
+            msg: msg.substring(0, 19),
+            msg2: msg.substring(19, 39),
+            msg3: msg.substring(39, 59),
+            msg4: msg.substring(59, 79),
+            msg5: msg.substring(79, 99),
+            msg6: msg.substring(99, 119),
+            msg7: msg.substring(119, 139),
+            msg8: msg.substring(139)
+        };
+        fbRef.ref('msg').push(data);
+        proceed();
     }
     else if (validEmail) {
         document.getElementById("validEmail").innerText = "Please Enter a Message";
