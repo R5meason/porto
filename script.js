0cc3135439375e05047ed630e69dd46771dc3bb5
R5meason <melynda.eason@gmail.com>
2016-12-21 14:15:20 -0800

    fixed sizing issues

--- script.js
+++ script.js
@@ -34,11 +34,22 @@ firebase.initializeApp(config);
 var fbRef = firebase.database();
 
 var validEmail = false;
+var nameEntered = false;
+
+var nameCheck = function () {
+    if ($('#name').val().length < 2) {
+        document.getElementById("validEmail").innerHTML = "Please Enter Your Name";
+        nameEntered = false;
+    }
+    else {
+        nameEntered = true;
+    }
+};
 
 var emailCheck = function () {
     if (!$('#emailAddy').val().includes("@") || !$('#emailAddy').val().includes(".com")) {
-        console.log("no");
         document.getElementById("validEmail").innerHTML = "Please Enter a Valid Email Address";
+        validEmail = false;
     }
     else {
         validEmail = true;
@@ -46,109 +57,121 @@ var emailCheck = function () {
 };
 
 var proceed = function () {
+    document.getElementById('name').value = "";
     document.getElementById('emailAddy').value = "";
     document.getElementById('msg').value = "";
     document.getElementById("validEmail").innerText = "Your Message Has Been Sent!";
     validEmail = false;
-    validMsg = false;
+    nameEntered = false;
 };
 
-var sendMsg = function (em, msg) {
-    emailCheck();
+var sendMsg = function (name, em, msg) {
+    nameCheck();
+    if (nameEntered) {
+        emailCheck();
 
-    if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 21) {
-        var data = {
-            em: em,
-            msg: msg
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 41) {
-        var data = {
-            em: em,
-            msg: msg.substring(0, 19),
-            msg2: msg.substring(19)
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 61) {
-        var data = {
-            em: em,
-            msg: msg.substring(0, 19),
-            msg2: msg.substring(19, 39),
-            msg3: msg.substring(39)
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 81) {
-        var data = {
-            em: em,
-            msg: msg.substring(0, 19),
-            msg2: msg.substring(19, 39),
-            msg3: msg.substring(39, 59),
-            msg4: msg.substring(59)
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 101) {
-        var data = {
-            em: em,
-            msg: msg.substring(0, 19),
-            msg2: msg.substring(19, 39),
-            msg3: msg.substring(39, 59),
-            msg4: msg.substring(59, 79),
-            msg5: msg.substring(79)
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 121) {
-        var data = {
-            em: em,
-            msg: msg.substring(0, 19),
-            msg2: msg.substring(19, 39),
-            msg3: msg.substring(39, 59),
-            msg4: msg.substring(59, 79),
-            msg5: msg.substring(79, 99),
-            msg6: msg.substring(99)
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 141) {
-        var data = {
-            em: em,
-            msg: msg.substring(0, 19),
-            msg2: msg.substring(19, 39),
-            msg3: msg.substring(39, 59),
-            msg4: msg.substring(59, 79),
-            msg5: msg.substring(79, 99),
-            msg6: msg.substring(99, 119),
-            msg7: msg.substring(119)
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 161) {
-        var data = {
-            em: em,
-            msg: msg.substring(0, 19),
-            msg2: msg.substring(19, 39),
-            msg3: msg.substring(39, 59),
-            msg4: msg.substring(59, 79),
-            msg5: msg.substring(79, 99),
-            msg6: msg.substring(99, 119),
-            msg7: msg.substring(119, 139),
-            msg8: msg.substring(139)
-        };
-        fbRef.ref('msg').push(data);
-        proceed();
-    }
-    else if (validEmail) {
-        document.getElementById("validEmail").innerText = "Please Enter a Message";
+        if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 21) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 41) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg.substring(0, 19),
+                msg2: msg.substring(19)
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 61) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg.substring(0, 19),
+                msg2: msg.substring(19, 39),
+                msg3: msg.substring(39)
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 81) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg.substring(0, 19),
+                msg2: msg.substring(19, 39),
+                msg3: msg.substring(39, 59),
+                msg4: msg.substring(59)
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 101) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg.substring(0, 19),
+                msg2: msg.substring(19, 39),
+                msg3: msg.substring(39, 59),
+                msg4: msg.substring(59, 79),
+                msg5: msg.substring(79)
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 121) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg.substring(0, 19),
+                msg2: msg.substring(19, 39),
+                msg3: msg.substring(39, 59),
+                msg4: msg.substring(59, 79),
+                msg5: msg.substring(79, 99),
+                msg6: msg.substring(99)
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 141) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg.substring(0, 19),
+                msg2: msg.substring(19, 39),
+                msg3: msg.substring(39, 59),
+                msg4: msg.substring(59, 79),
+                msg5: msg.substring(79, 99),
+                msg6: msg.substring(99, 119),
+                msg7: msg.substring(119)
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail && $('#msg').val().length > 0 && $('#msg').val().length < 161) {
+            var data = {
+                name: name,
+                em: em,
+                msg: msg.substring(0, 19),
+                msg2: msg.substring(19, 39),
+                msg3: msg.substring(39, 59),
+                msg4: msg.substring(59, 79),
+                msg5: msg.substring(79, 99),
+                msg6: msg.substring(99, 119),
+                msg7: msg.substring(119, 139),
+                msg8: msg.substring(139)
+            };
+            fbRef.ref('msg').push(data);
+            proceed();
+        }
+        else if (validEmail) {
+            document.getElementById("validEmail").innerText = "Please Enter a Message";
+        }
     }
 };
